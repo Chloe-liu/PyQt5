457e95ceec16d5155ae46ae943caebb949576ded
Chloe <Chloe.lau@foxmail.com>
2019-03-09 20:54:27 +0800

    experiment

---
+++ experiment/ui_prism.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+ 
+ 
+class Ex(QWidget):
+     
+    def __init__(self):
+        super().__init__()     
+        self.initUI()
+                 
+    def initUI(self):
+         
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))       
+        
+         
+         
+if __name__ == '__main__':
+     
+    app = QApplication(sys.argv)
+    ex = Ex()
+    sys.exit(app.exec_())  
